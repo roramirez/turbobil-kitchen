ed130b4aed313f6b64bdfccdb7ab7ef32cc15981
Rodrigo Ramírez Norambuena <decipher.hk@gmail.com>
2014-11-02 23:06:40 +0000

    format metadata

--- my-cookbooks/turbobil/metadata.rb
+++ my-cookbooks/turbobil/metadata.rb
@@ -1,12 +1,13 @@
-name 'turbobil'
-maintainer 'Rodrigo Ramirez Norambuena'
-maintainer_email 'rodrigo@blackhole.cl'
-license 'Apache 2.0'
-description 'Installs and configures TurboBil'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.1'
-recipe 'turbobil', 'Main configuration'
+name              'turbobil'
+maintainer        'Rodrigo Ramirez Norambuena'
+maintainer_email  'rodrigo@blackhole.cl'
+license           'Apache 2.0'
+description       'Installs and configures TurboBil'
+version           '0.0.1'
 
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
+recipe 'turbobil','Main configuration'
 depends 'apt'
 depends 'ruby_build'
 depends 'user'
